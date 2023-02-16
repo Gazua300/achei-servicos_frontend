047724ae29ddee45e89d66463d8505d1d39c262f
Gazua300 <mesmo245@gmail.com>
2023-02-16 15:14:45 -0300

    adicionando API do serviço onrender

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'https://labeninja-production.up.railway.app'
+// export const BASE_URL = 'https://labeninja-production.up.railway.app'
+export const BASE_URL = 'https://achei-servicos-server.onrender.com'
 
